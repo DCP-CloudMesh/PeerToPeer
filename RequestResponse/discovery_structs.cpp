17d2d2ab2b1121188dd351e273b7b40e592f5eca
Wuyue (Tony) Sun <sunwuyue@live.com>
2023-11-06 15:50:38 -0500

    Add serialization and data defs

---
+++ RequestResponse/discovery_structs.cpp
@@ -0,0 +1,36 @@
+#include "discovery_structs.h"
+
+std::string serializeDiscoveryRequest(const DiscoveryRequest& request) { 
+    std::string serializedData = request.req_uuid + "|" +
+                     request.req_ipAddress.to_string() + "|" +
+                     std::to_string(request.req_port) + "|" +
+                     std::to_string(request.peers_requested);
+}
+
+DiscoveryRequest deserializeDiscoveryRequest(const std::string& serializedData) {
+    DiscoveryRequest request;
+    size_t pos = 0;
+    size_t prevPos = 0;
+    for (int i = 0; i < 4; ++i) {
+        pos = serializedData.find('|', prevPos);
+        std::string token = serializedData.substr(prevPos, pos - prevPos);
+        prevPos = pos + 1;
+        switch (i) {
+            case 0:
+                request.req_uuid = token;
+                break;
+            case 1:
+                request.req_ipAddress = boost::asio::ip::address_v4::from_string(token);
+                break;
+            case 2:
+                request.req_port = std::stoi(token);
+                break;
+            case 3:
+                request.peers_requested = std::stoi(token);
+                break;
+        }
+    }
+    return request;
+}
+
+
---
+++ RequestResponse/discovery_structs.h
@@ -0,0 +1,19 @@
+#ifndef _DISCOVERY_
+#define _DISCOVERY_
+
+#include <string>
+
+struct DiscoveryRequest {
+    std::string req_uuid;
+    boost::asio::ip::address req_ipAddress;
+    unsigned short req_port;
+    int peers_requested;
+};
+
+struct DiscoveryResponse {
+    std::unordered_map<std::string, boost::asio::ip::address> providerPeers;
+};
+
+
+
+#endif 
