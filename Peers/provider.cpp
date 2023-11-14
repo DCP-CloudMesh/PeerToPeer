f021cb78c089f43425c23f2f88d4fe0425737941
Wuyue (Tony) Sun <sunwuyue@live.com>
2023-11-14 16:52:56 -0500

    Add ngrok call

--- Peers/provider.cpp
+++ Peers/provider.cpp
@@ -1,1 +1,33 @@
 #include "provider.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+string Provider::getPublicIPAddr() {
+    const char* command = "python3 ngrok.py";
+
+    // Open a pipe to capture the output
+    FILE* pipe = popen(command, "r");
+    if (!pipe) {
+        std::cerr << "Error opening pipe." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Read the output of the Python script
+    char buffer[256];
+    std::string result = "";
+    while (!feof(pipe)) {
+        if (fgets(buffer, 256, pipe) != nullptr)
+            result += buffer;
+    }
+
+    // Close the pipe
+    pclose(pipe);
+
+    // Print the captured output
+    std::cout << "Python script output:\n" << result << std::endl;
+
+    return result;
+}
