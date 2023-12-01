903b71eccf2ce73d655d9ee6eeb1f9259e8ad10d
jordanmao <jordanmao2@gmail.com>
2023-12-01 12:37:19 -0500

    minor change: fix unsigned int comparison warning

--- src/utility.cpp
+++ src/utility.cpp
@@ -69,7 +69,7 @@ void close_ngrok_forwarding() { system("pkill ngrok"); }
 
 string vectorToString(vector<int> vec) {
     stringstream ss;
-    for (int i = 0; i < vec.size(); i++) {
+    for (unsigned int i = 0; i < vec.size(); i++) {
         ss << vec[i];
         if (i != vec.size() - 1) {
             ss << ",";
